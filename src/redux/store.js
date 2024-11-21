73eadab39ac9268fd4a117f2e60ce09d4a8b9057
Sahil Khilari <sahilkhilari16@gmail.com>
2024-11-21 15:49:36 +0530

    Changes in Backend/Frontend submodule

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,4 +19,5 @@ const store = configureStore({
 
 
 export default store;
-export const server = 'https://skillcanvas-1.onrender.com/api/v1'
+// export const server = 'https://skillcanvas-1.onrender.com/api/v1'
+export const server = "http://localhost:4000/api/v1"; // Ensure this is defined.
